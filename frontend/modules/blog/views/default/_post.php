20c5e1b47af1a30f6f4f4cc2ddf1cc7a14108ce5
xNPC <mix-88@list.ru>
2016-04-04 16:27:31 +0600

    fix views

--- frontend/modules/blog/views/default/_post.php
+++ frontend/modules/blog/views/default/_post.php
@@ -9,25 +9,24 @@ use yii\helpers\HtmlPurifier;
 ?>
 <h2>
     <?= Html::a(Html::encode($post->title),
-        ['post/view', 'id'=>Html::encode($post->id)]);
-    ?>
+        ['post/view', 'id' => Html::encode($post->id)]
+    ); ?>
 </h2>
 <div>
-    <?= Yii::t('blog', 'Category:');?>
+    <?= Yii::t('blog', 'Category:'); ?>
     <?= Html::a(Html::encode($post->category->name),
-        ['/blog/default/category', 'category'=>Html::encode($post->category->alias)]);
-    ?>
-    ,
-    <?=Yii::t('blog', 'Date'); ?>: <?=Yii::$app->formatter->asDate($post->updated_at, 'long');?>
+        ['/blog/default/category', 'category' => Html::encode($post->category->alias)]
+    ); ?>,
+    <?= Yii::t('blog', 'Date:'); ?> <?= Yii::$app->formatter->asDate($post->updated_at, 'long'); ?>
 </div>
 <div class="post-anons">
     <?= HtmlPurifier::process($post->anons); ?>
 </div>
 <div class="read-more">
     <?= Html::a(Yii::t('blog', 'Read more...'),
-        ['post/view', 'id'=>Html::encode($post->id)]);
-    ?>
+        ['post/view', 'id' => Html::encode($post->id)]
+    ); ?>
 </div>
 <div class="post-info">
-    <?= Yii::t('blog','Author:');?> <?=Html::encode($post->author->username);?>
+    <?= Yii::t('blog','Author:'); ?> <?= Html::encode($post->author->username); ?>
 </div>
--- frontend/modules/blog/views/post/view.php
+++ frontend/modules/blog/views/post/view.php
@@ -12,24 +12,26 @@ $this->params['breadcrumbs'][] = ['label' => Html::encode($post->category->name)
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?= Html::a(Yii::t('blog', 'Update post'), ['/blog/post/update', 'id' => $post->id], ['class' => 'btn btn-primary']); ?>
-<h1><?=Html::encode($post->title);?></h1>
-<p>
-    <?=Yii::t('blog', 'Author'); ?>: <?=Html::encode($post->author->username);?>,
-    <?=Yii::t('blog', 'Category'); ?>: <?=Html::a(
-                Html::encode($post->category->name),
-                ['/blog/default/category', 'category'=>Html::encode($post->category->alias)]
-            );?>,
-    <?=Yii::t('blog', 'Date'); ?>: <?=Yii::$app->formatter->asDate($post->updated_at, 'long');?>
-</p>
-<p>
-    <?=HtmlPurifier::process($post->content);?>
-</p>
-
-<p><?=Yii::t('blog', 'Tags'); ?>:
+<h1>
+    <?= Html::encode($post->title); ?>
+</h1>
+<div>
+    <?= Yii::t('blog', 'Category:'); ?>
+    <?= Html::a(Html::encode($post->category->name),
+        ['/blog/default/category', 'category' => Html::encode($post->category->alias)]
+    ); ?>,
+    <?= Yii::t('blog', 'Date:'); ?> <?= Yii::$app->formatter->asDate($post->updated_at, 'long'); ?>
+</div>
+<div class="post-content">
+    <?= HtmlPurifier::process($post->content); ?>
+</div>
+<div class="post-tags">
+    <?= Yii::t('blog', 'Tags:'); ?>
     <?php foreach ($post->tags as $tag): ?>
-        <?=Html::encode($tag->name);?>
+        <?= Html::encode($tag->name); ?>
     <?php endforeach; ?>
-
-</p>
-
+</div>
+<div class="post-info">
+    <?= Yii::t('blog', 'Author:'); ?> <?= Html::encode($post->author->username); ?>
+</div>
 
